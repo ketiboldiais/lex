0c82faec02cbd48c4d00fa14f5c371057ba7ec1b
ketiboldiais <numerstack@gmail.com>
2024-11-09 13:09:33 -0600

    add main file for weave

---
+++ src/weave/main.ts
@@ -0,0 +1,52 @@
+enum TOKEN {
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FOR,
+    FN,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+    ERROR,
+    EOF
+}
+
+function scan(source: string) {
+    let start = 0;
+    let current = 0;
+    let line = 1;
+}
+
+export const scanner = (source:string) => {
+    return source;
+}
